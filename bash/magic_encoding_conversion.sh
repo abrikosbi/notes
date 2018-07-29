f254c63a33600d23f8ca36940c4c1b1f5e86390b
koskiszz <koskiszz@gmail.com>
2018-07-29 23:06:17 +0700

    мегакоманда конвертации кодировки на маке, которую можно использовать для переименования файлов

--- bash/magic_encoding_conversion.sh
+++ bash/magic_encoding_conversion.sh
@@ -14,3 +14,6 @@ done
 # выжимка
 # echo äïçï÷ïò | iconv -f utf-8 -t cp1252 | iconv -f koi8-r -t utf-8
 # echo "Êàêèå-òî êðàêîçÿáðû" | iconv -t latin1 | iconv -f cp1251 -t utf-8
+
+# Конвертация нормализованных для мака символов
+# echo e<0300>Æ§£Æ‚Æ¢™† ™ ™„‡·„ | iconv -f utf-8-mac -t macRoman | iconv -f cp866 -t utf-8
